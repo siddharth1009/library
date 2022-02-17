dfaad785db2a47ac72022c195a618ef1e9a5cc86
Siddharth Bhusnur <59989501+siddharth1009@users.noreply.github.com>
2022-02-18 01:30:26 +0530

    changes yar

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
-const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://sid:pawarpastey@cluster0.bqqxn.mongodb.net/library?retryWrites=true&w=majority")
-.then(()=>console.log("connection established mongo"));
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+app.use(compression()); //Compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
